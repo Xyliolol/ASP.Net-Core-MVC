4dfcdde8c4f75abc423dd08d9e29dc61fec9e66d
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2022-06-03 10:33:15 +0300

    Вывеk список товаров из каталога на странице catalog/products Добавил страницу добавления новых товаров в каталог

--- MVCApp/Program.cs
+++ MVCApp/Program.cs
@@ -1,7 +1,10 @@
+using MVCApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<AddProduct>();
 
 var app = builder.Build();
 
